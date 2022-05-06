e6c86619d8d50395b27269fc012324844f1c3d9b
Guillermo Rodriguez <guillermo@telos.net>
2022-05-05 21:51:44 -0300

    Add action data serialization

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,24 @@ import {
     ShipTransactionTrace
 } from './types/ship';
 
+import * as eosioEvmAbi from './abis/evm.json';
+import * as eosioTokenAbi from './abis/token.json'
+
+import { Serialize , RpcInterfaces } from 'eosjs';
+
+const encoder = new TextEncoder;
+const decoder = new TextDecoder;
+
+
+function getContract(contractAbi: RpcInterfaces.Abi) {
+    const types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), contractAbi)
+    const actions = new Map()
+    for (const { name, type } of contractAbi.actions) {
+        actions.set(name, Serialize.getType(types, type))
+    }
+    return { types, actions }
+}
+
 const endpoint = 'ws://api2.hosts.caleos.io:8999';
 const startBlock = 180698860;
 const stopBlock = 0xffffffff;
@@ -14,14 +32,55 @@ const reader = new StateHistoryBlockReader(endpoint);
 
 reader.setOptions({
     min_block_confirmation: 1,
-    ds_threads: 32,
+    ds_threads: 16,
     allow_empty_deltas: false,
     allow_empty_traces: false,
     allow_empty_blocks: false
 });
 
+
+const evmContract = getContract(eosioEvmAbi.abi);
+const tokenContract = getContract(eosioTokenAbi.abi);
+
+function deserialize(types: Map<string, Serialize.Type>, array: Uint8Array) {
+    const buffer = new Serialize.SerialBuffer(
+        { textEncoder: encoder, textDecoder: decoder, array });
+
+    let result = Serialize.getType(types, "transaction")
+        .deserialize(buffer, new Serialize.SerializerState({ bytesAsUint8Array: true }));
+
+    return result;
+}
+
 async function consumer(resp: ShipBlockResponse): Promise<void> {
-    console.log(resp);
+    resp.block.transactions.forEach((tx) => {
+        if (tx.trx[0] == "packed_transaction") {
+            const packed_trx = tx.trx[1].packed_trx;
+            const trx = deserialize(reader.types, packed_trx);
+            
+            trx.actions.forEach((action: Serialize.Action) => {
+                let contract = null;
+                if (action.account == "eosio.evm") {
+                    contract = evmContract;
+                } else if (action.account == "eosio.token") {
+                    contract = tokenContract;
+                } else {
+                    return;
+                }
+
+                const result = Serialize.deserializeActionData(
+                    contract,
+                    action.account,
+                    action.name,
+                    action.data,
+                    encoder,
+                    decoder);
+
+                console.log(trx);
+                console.log(result);
+            });
+        }
+    });
 }
 
 reader.consume(consumer);
