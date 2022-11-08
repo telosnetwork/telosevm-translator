d04f9e8d9ad406591a875ce314a0f9875c010750
Guillermo Rodriguez <guillermo@telos.net>
2022-11-08 08:08:46 -0300

    Add logging on unhandled exceptions

--- src/indexer.ts
+++ src/indexer.ts
@@ -31,6 +31,12 @@ const BN = require('bn.js');
 const sleep = (ms: number) => new Promise( res => setTimeout(res, ms));
 
 
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+  // @ts-ignore
+  console.log(reason.stack)
+});
 
 export class TEVMIndexer {
     endpoint: string;
