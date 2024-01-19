ab05c74806a4d873374ff74bb95455cc0a098e20
Guillermo Rodriguez <guillermo@telos.net>
2024-01-19 04:25:32 -0300

    Reindex tests cleanup
    Improve reindex tests entrypoints and add more config params
    Survive reaching end of index and ending on BlockScroller
    Improve logic that builds blockTxs lists to yield per block on scroller
    Support undefined gasUsed on source doc

--- scripts/tests/test30MReindex.mjs
+++ scripts/tests/test30MReindex.mjs
@@ -7,23 +7,26 @@ import {Command} from "commander";
  */
 const program = new Command();
 program
-    .option('-b, --totalBlocks [totalBlocks]', 'Set a specific amount of blocks to sync')
-    .option('-L, --limit [limit]', 'Number of documents per elasticdump import batch')
+    .option('-b, --totalBlocks [totalBlocks]', 'Set a specific amount of blocks to sync', '29999963')
+    .option('-p, --purge', 'Set a specific amount of blocks to sync', false)
+    .option('-L, --limit [limit]', 'Number of documents per elasticdump import batch', '4000')
+    .option('-T, --timeout [timeout m]', 'Timeout in minutes', '100')
     .action(async (options) => {
         await translatorESReindexVerificationTest({
             title: '30Mil Index Change Test',
-            timeout: 20 * 60,  // 20 minutes in seconds
+            timeout: parseInt(options.timeout, 10) * 60,
             esDumpName: 'telos-mainnet-30M-ebr',
             elastic: {
                 host: 'http://127.0.0.1:9200',
-                esDumpLimit: options.limit ? parseInt(options.limit, 10) : 4000
+                esDumpLimit: parseInt(options.limit, 10),
+                purge: options.purge
             },
             srcPrefix: 'telos-mainnet-verification',
             dstPrefix: 'telos-mainnet-reindex',
             indexVersion: 'v1.5',
             template: 'config.mainnet.json',
             startBlock: 36,
-            totalBlocks: options.totalBlocks ? parseInt(options.totalBlocks, 10) : 29999963,
+            totalBlocks: parseInt(options.totalBlocks, 10),
             evmPrevHash: '',
             evmValidateHash: '36fe7024b760365e3970b7b403e161811c1e626edd68460272fcdfa276272563',
             esDumpSize: 10000,
