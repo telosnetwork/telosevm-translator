3f5b1d68065205df966540e69f516cd78338ee0b
Guillermo Rodriguez <guillermo@telos.net>
2024-04-26 12:56:29 -0300

    Remove faulty env var entrypoint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { readFileSync } from 'node:fs';
 import cloneDeep from 'lodash.clonedeep';
 import { mergeDeep } from './utils/misc.js';
 import { Command } from 'commander';
-import { cleanEnv, str, num, bool } from 'envalid';
 
 const program = new Command();
 
@@ -22,35 +21,6 @@ program
         undefined
     )
     .action(async (options) => {
-        const env = cleanEnv(process.env, {
-            LOG_LEVEL: str({ default: DEFAULT_CONF.logLevel }),
-            READER_LOG_LEVEL: str({ default: DEFAULT_CONF.readerLogLevel }),
-            CHAIN_NAME: str({ default: DEFAULT_CONF.chainName }),
-            CHAIN_ID: num({ default: DEFAULT_CONF.chainId }),
-            TELOS_ENDPOINT: str({ default: DEFAULT_CONF.endpoint }),
-            TELOS_REMOTE_ENDPOINT: str({ default: DEFAULT_CONF.remoteEndpoint }),
-            TELOS_WS_ENDPOINT: str({ default: DEFAULT_CONF.wsEndpoint }),
-            EVM_BLOCK_DELTA: num({ default: DEFAULT_CONF.evmBlockDelta }),
-            EVM_PREV_HASH: str({ default: DEFAULT_CONF.evmPrevHash }),
-            EVM_VALIDATE_HASH: str({ default: DEFAULT_CONF.evmValidateHash }),
-            INDEXER_START_BLOCK: num({ default: DEFAULT_CONF.startBlock }),
-            INDEXER_STOP_BLOCK: num({ default: DEFAULT_CONF.stopBlock }),
-            BROADCAST_HOST: str({ default: DEFAULT_CONF.broadcast.wsHost }),
-            BROADCAST_PORT: num({ default: DEFAULT_CONF.broadcast.wsPort }),
-            ELASTIC_NODE: str({ default: DEFAULT_CONF.elastic.node }),
-            ELASTIC_USERNAME: str({ default: DEFAULT_CONF.elastic.auth.username }),
-            ELASTIC_PASSWORD: str({ default: DEFAULT_CONF.elastic.auth.password }),
-            ELASTIC_TIMEOUT: num({ default: DEFAULT_CONF.elastic.requestTimeout }),
-            ELASTIC_DUMP_SIZE: num({ default: DEFAULT_CONF.perf.elasticDumpSize }),
-            ELASTIC_DOCS_PER_INDEX: num({ default: DEFAULT_CONF.elastic.docsPerIndex }),
-            ELASTIC_SHARD_NUM: num({ default: DEFAULT_CONF.elastic.numberOfShards }),
-            ELASTIC_REPLICA_NUM: num({ default: DEFAULT_CONF.elastic.numberOfReplicas }),
-            READER_WORKER_AMOUNT: num({ default: DEFAULT_CONF.perf.readerWorkerAmount }),
-            EVM_WORKER_AMOUNT: num({ default: DEFAULT_CONF.perf.evmWorkerAmount }),
-            IRREVERSIBLE_ONLY: bool({ default: DEFAULT_CONF.irreversibleOnly }),
-            BLOCK_HISTORY_SIZE: num({ default: DEFAULT_CONF.blockHistorySize }),
-        });
-
         const conf: IndexerConfig = cloneDeep(DEFAULT_CONF);
 
         try {
@@ -58,40 +28,26 @@ program
             mergeDeep(conf, userConf);
         } catch (e) {}
 
-        conf.runtime.skipIntegrityCheck = options.skipIntegrityCheck;
-        conf.runtime.onlyDBCheck = options.onlyDBCheck;
-        conf.runtime.gapsPurge = options.gapsPurge;
-        conf.runtime.skipStartBlockCheck = options.skipStartBlockCheck;
-        conf.runtime.skipRemoteCheck = options.skipRemoteCheck;
-        conf.runtime.reindexInto = options.reindexInto;
-        conf.runtime.trimFrom = options.trimFrom ? parseInt(options.trimFrom, 10) : undefined;
+        if (options.skipIntegrityCheck)
+            conf.runtime.skipIntegrityCheck = options.skipIntegrityCheck;
 
-        conf.logLevel = env.LOG_LEVEL;
-        conf.readerLogLevel = env.READER_LOG_LEVEL;
-        conf.chainName = env.CHAIN_NAME;
-        conf.chainId = env.CHAIN_ID;
-        conf.endpoint = env.TELOS_ENDPOINT;
-        conf.remoteEndpoint = env.TELOS_REMOTE_ENDPOINT;
-        conf.wsEndpoint = env.TELOS_WS_ENDPOINT;
-        conf.evmBlockDelta = env.EVM_BLOCK_DELTA;
-        conf.evmPrevHash = env.EVM_PREV_HASH;
-        conf.evmValidateHash = env.EVM_VALIDATE_HASH;
-        conf.startBlock = env.INDEXER_START_BLOCK;
-        conf.stopBlock = env.INDEXER_STOP_BLOCK;
-        conf.broadcast.wsHost = env.BROADCAST_HOST;
-        conf.broadcast.wsPort = env.BROADCAST_PORT;
-        conf.elastic.node = env.ELASTIC_NODE;
-        conf.elastic.auth.username = env.ELASTIC_USERNAME;
-        conf.elastic.auth.password = env.ELASTIC_PASSWORD;
-        conf.elastic.requestTimeout = env.ELASTIC_TIMEOUT;
-        conf.elastic.docsPerIndex = env.ELASTIC_DOCS_PER_INDEX;
-        conf.perf.readerWorkerAmount = env.READER_WORKER_AMOUNT;
-        conf.perf.evmWorkerAmount = env.EVM_WORKER_AMOUNT;
-        conf.perf.elasticDumpSize = env.ELASTIC_DUMP_SIZE;
-        conf.irreversibleOnly = env.IRREVERSIBLE_ONLY;
-        conf.blockHistorySize = env.BLOCK_HISTORY_SIZE;
-        conf.elastic.numberOfShards = env.ELASTIC_SHARD_NUM;
-        conf.elastic.numberOfReplicas = env.ELASTIC_REPLICA_NUM;
+        if (options.onlyDBCheck)
+            conf.runtime.onlyDBCheck = options.onlyDBCheck;
+
+        if (options.gapsPurge)
+            conf.runtime.gapsPurge = options.gapsPurge;
+
+        if (options.skipStartBlockCheck)
+            conf.runtime.skipStartBlockCheck = options.skipStartBlockCheck;
+
+        if (options.skipRemoteCheck)
+            conf.runtime.skipRemoteCheck = options.skipRemoteCheck;
+
+        if (options.reindexInto)
+            conf.runtime.reindexInto = options.reindexInto;
+
+        if (options.trimFrom)
+            conf.runtime.trimFrom = options.trimFrom ? parseInt(options.trimFrom, 10) : undefined;
 
         const indexer = new TEVMIndexer(conf);
         try {
